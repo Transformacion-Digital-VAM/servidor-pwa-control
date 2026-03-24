6cdadbd1d9f9dd076b0e12b92aa9f2c4ec86e00f
tacomon <carc1403@gmail.com>
2026-03-24 12:15:40 -0600

    Correccion para Puppeteer

--- controllers/hcontrol.controller.js
+++ controllers/hcontrol.controller.js
@@ -15,9 +15,14 @@ const getBrowser = async () => {
                 '--no-sandbox', 
                 '--disable-setuid-sandbox',
                 '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process',
                 '--disable-gpu'
             ]
         });
+
     }
     return _browser;
 };
