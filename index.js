76cf97cf697cda71ef3457e31cb021fd98ad3753
tacomon <carc1403@gmail.com>
2026-04-17 03:56:34 -0600

    recuperacion Solidarios

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(cors({
   origin: [
     'https://pwa-hcontrol-vam.web.app',
     'http://localhost:4200',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    '*'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
